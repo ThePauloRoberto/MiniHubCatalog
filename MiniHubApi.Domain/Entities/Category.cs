cea3c900c8a8e102c828efe660be97c885c80761
Paulo Roberto Antunes de Carvalho <paulorantunes13@hotmail.com>
2026-01-13 16:03:54 -0300

    feat(domain): add Category entity with basic properties
    - Create Category class with Id and Name properties
    - Add required data annotations

---
+++ MiniHubApi.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniHubApi.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    public Item Item { get; set; }
+    
+    public Category()
+    {}
+}
---
+++ MiniHubApi.Domain/Entities/Tag.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniHubApi.Domain.Entities;
+
+public class Tag
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public List<Item> Items { get; set; }
+}
