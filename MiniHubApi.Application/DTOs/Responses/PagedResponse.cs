d94ab31404c5ade2098587b58432c159e6564b05
Paulo Roberto Antunes de Carvalho <paulorantunes13@hotmail.com>
2026-01-15 15:49:48 -0300

    feat: add query parameter DTOs for standardized API requests
    Query Parameter DTOs:
    - Create ItemQueryParams for item filtering and pagination
    - Create TagQueryParams for tag filtering
    - Add validation attributes (Range, RegularExpression)
    - Standardize parameter names across all endpoints

---
+++ MiniHubApi.Application/DTOs/Responses/PagedResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MiniHubApi.Application.DTOs.Responses;
+
+public class PagedResponse<T>
+{
+    public List<T> Data { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+    
+    public bool HasPrevious => Page > 1;
+    public bool HasNext => Page < TotalPages;
+
+    public PagedResponse(
+        List<T> data, 
+        int page, 
+        int pageSize, 
+        int totalCount)
+    {
+        Data = data;
+        Page = page;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+        TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+    }
+}
---
+++ MiniHubApi.Application/Utils/ItemQueryParams.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniHubApi.Application.Utils;
+
+public class ItemQueryParams
+{
+    public string? Search { get; set; }
+    public string OrderBy { get; set; } = "nome";
+    public string OrderDirection { get; set; } = "ASC";
+    
+    [Range(1, int.MaxValue)]
+    public int Page { get; set; } = 1;
+    
+    [Range(1, 100)]
+    public int PageSize { get; set; } = 10;
+}
