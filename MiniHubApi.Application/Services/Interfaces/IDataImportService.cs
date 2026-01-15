faeacad023157d1fd892c025af4a7980365f2216
Paulo Roberto Antunes de Carvalho <paulorantunes13@hotmail.com>
2026-01-14 21:54:56 -0300

    feat: define service interfaces (IDataImportService, IExternalApiService)

---
+++ MiniHubApi.Application/Services/Interfaces/IDataImportService.cs
@@ -0,0 +1,19 @@
+﻿namespace MiniHubApi.Application.Services.Interfaces;
+
+public interface IDataImportService
+{
+    Task<ImportResult> ImportCategoriesAsync();
+    Task<ImportResult> ImportProductAsync();
+}
+
+public class ImportResult
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public int Created { get; set; }
+    public int Updated { get; set; }
+    public int Failed { get; set; }
+    public List<string> Errors { get; set; } = new();
+    public DateTime StartedAt { get; set; }
+    public DateTime CompletedAt { get; set; }
+}
---
+++ MiniHubApi.Application/Services/Interfaces/IExternalApiService.cs
@@ -0,0 +1,10 @@
+﻿using MiniHubApi.Application.DTOs.External;
+
+namespace MiniHubApi.Application.Services.Interfaces;
+
+public interface IExternalApiService
+{
+    Task<List<ExternalProductDto>> GetAllProductsAsync();
+    
+    Task<List<ExternalCategoryDto>> GetCategoriesAsync();
+}
