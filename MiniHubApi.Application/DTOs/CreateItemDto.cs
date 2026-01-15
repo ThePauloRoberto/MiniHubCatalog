48f173012df004c2d14a8273dccd442d26a4f614
Paulo Roberto Antunes de Carvalho <paulorantunes13@hotmail.com>
2026-01-14 21:51:39 -0300

    feat: add CreateItemDto and UpdateItemDto

---
+++ MiniHubApi.Application/DTOs/CreateItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniHubApi.Application.DTOs;
+
+public class CreateItemDto
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+    public decimal Preco { get; set; }
+    public bool Ativo { get; set; } = true;
+    public int Estoque { get; set; } = 0;
+    public int? CategoryId { get; set; }
+    public List<Guid> TagIds { get; set; } = new(); 
+}
---
+++ MiniHubApi.Application/DTOs/UpdateItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniHubApi.Application.DTOs;
+
+public class UpdateItemDto
+{
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal? Preco { get; set; } 
+    public bool? Ativo { get; set; } 
+    public int? Estoque { get; set; }
+    public int? CategoryId { get; set; } 
+    public List<Guid>? TagIds { get; set; }  
+}
