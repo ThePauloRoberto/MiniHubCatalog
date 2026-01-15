a998548348e5ee76ae3f5dcec5dd99b6d8960ff8
Paulo Roberto Antunes de Carvalho <paulorantunes13@hotmail.com>
2026-01-14 21:40:20 -0300

    refactor(entity): modify Category entity relationship configuration

--- MiniHubApi.Domain/Entities/Category.cs
+++ MiniHubApi.Domain/Entities/Category.cs
@@ -4,8 +4,8 @@ public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    
-    public Item Item { get; set; }
+    public string? ExternalId { get; set; }
+    public ICollection<Item> Items { get; set; } = new List<Item>();
     
     public Category()
     {}
