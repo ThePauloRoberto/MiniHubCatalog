17c8030b7710caf52c062dae6c40adafdb172e69
Paulo Roberto Antunes de Carvalho <paulorantunes13@hotmail.com>
2026-01-14 21:48:00 -0300

    refactor(db): update entity configurations and fluent validations

--- MiniHubApi.Infrastructure/Data/ApplicationDbContext.cs
+++ MiniHubApi.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using MiniHubApi.Domain.Entities;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace MiniHubApi.Infrastructure.Data
 {
@@ -31,12 +31,31 @@ namespace MiniHubApi.Infrastructure.Data
                 entity.Property(i => i.Preco).HasColumnType("DECIMAL(10,2)").IsRequired();
                 entity.Property(i => i.Ativo).HasColumnType("TINYINT(1)").IsRequired().HasDefaultValue(true);
                 
+                entity.Property(i => i.Estoque)
+                    .IsRequired()
+                    .HasDefaultValue(0);
+            
+                entity.Property(i => i.ExternalId)
+                    .HasColumnType("VARCHAR(50)")
+                    .IsRequired(false);
+            
+                entity.Property(i => i.CategoryExternalId)
+                    .HasColumnType("VARCHAR(50)")
+                    .IsRequired(false);
+                
                 entity.Property(i => i.CategoryId)
                     .IsRequired(false);
                 
+                entity.HasIndex(i => i.ExternalId)
+                    .IsUnique()
+                    .HasDatabaseName("IX_Items_ExternalId");
+            
+                entity.HasIndex(i => i.CategoryExternalId)
+                    .HasDatabaseName("IX_Items_CategoryExternalId");
+                
                 entity.HasOne(i => i.Categoria)
-                    .WithOne(c => c.Item)
-                    .HasForeignKey<Item>(i => i.CategoryId)
+                    .WithMany(c => c.Items)  
+                    .HasForeignKey(i => i.CategoryId)
                     .IsRequired(false)
                     .OnDelete(DeleteBehavior.SetNull);
                 
@@ -60,7 +79,20 @@ namespace MiniHubApi.Infrastructure.Data
             {
                 entity.ToTable("Categories");
                 entity.HasKey(c => c.Id);
+                
+                entity.Property(c => c.Id)
+                    .ValueGeneratedOnAdd();
+                
                 entity.Property(c => c.Name).HasColumnType("VARCHAR(200)").IsRequired();
+                
+                entity.Property(c => c.ExternalId)
+                    .HasColumnType("VARCHAR(50)")
+                    .IsRequired(false);
+            
+                entity.HasIndex(c => c.ExternalId)
+                    .IsUnique()
+                    .HasDatabaseName("IX_Categories_ExternalId");
+                
                 entity.HasIndex(c => c.Id)
                     .IsUnique();
 
@@ -77,6 +109,23 @@ namespace MiniHubApi.Infrastructure.Data
 
             });
             
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = "Server=localhost;Port=3307;Database=MiniHubApi;Uid=myuser;Pwd=mypassword;";
+                  
+                optionsBuilder.UseMySql(
+                    connectionString,
+                    Microsoft.EntityFrameworkCore.ServerVersion.AutoDetect(connectionString),
+                    mysqlOptions =>
+                    {
+                        mysqlOptions.MigrationsAssembly("MiniHubApi.Infrastructure");
+                    });
+            }
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
