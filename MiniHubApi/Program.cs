508a803f938f2124fb18b757079e30f3aff5e8b8
Paulo Roberto Antunes de Carvalho <paulorantunes13@hotmail.com>
2026-01-14 21:56:22 -0300

    chore: register services in DI container

--- MiniHubApi/Program.cs
+++ MiniHubApi/Program.cs
@@ -1,22 +1,100 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using MiniHubApi.Application.Configuration;
+using MiniHubApi.Application.Services.Implementations;
+using MiniHubApi.Application.Services.Interfaces;
+using MiniHubApi.Infrastructure.Data;
+using MongoDB.Driver;
+using ServerVersion = Microsoft.EntityFrameworkCore.ServerVersion;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers() 
+    .AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.WriteIndented = true;
+});;
 builder.Services.AddSwaggerGen();
-//builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+builder.Services.Configure<ExternalApiSettings>(
+    builder.Configuration.GetSection(ExternalApiSettings.SectionName));
+builder.Services.AddHttpClient<IExternalApiService, ExternalApiService>();
+builder.Services.AddScoped<IDataImportService, DataImportService>();
+
+// Configure the HTTP request pipeline.
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
+
+var mySqlConnection = builder.Configuration.GetConnectionString("MySqlConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseMySql(
+        mySqlConnection,
+        ServerVersion.AutoDetect(mySqlConnection),
+        mysqlOptions =>
+        {
+            mysqlOptions.MigrationsAssembly("MiniHubApi.Infrastructure");
+        }
+        );
+    
+    if (builder.Environment.IsDevelopment())
+    {
+        options.EnableSensitiveDataLogging();
+        options.EnableDetailedErrors();
+        options.LogTo(Console.WriteLine, LogLevel.Information);
+    }
+});
+
+
+var mongoDbConnection = builder.Configuration.GetConnectionString("MongoDbConnection");
+
+builder.Services.AddSingleton<IMongoClient, MongoClient>(sp =>
+{
+    var settings = MongoClientSettings.FromConnectionString(mongoDbConnection);
+    return new MongoClient(settings);
+});
+
+builder.Services.AddScoped<IMongoDatabase>(sp =>
+{
+    var client = sp.GetRequiredService<IMongoClient>();
+    return client.GetDatabase("MiniHubAuditoria");
+});
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient("ExternalApi", client =>
+{
+    client.BaseAddress = new Uri("https://69657a38f6de16bde44a70f6.mockapi.io/:endpoint");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+    // Adicione headers de autenticação se necessário
+    // client.DefaultRequestHeaders.Add("Authorization", "Bearer token");
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    //app.MapOpenApi();
+    builder.Configuration.AddUserSecrets<Program>();
+    app.MapOpenApi();
 }
 
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
